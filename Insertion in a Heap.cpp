e83ce5b12ed22f694fb9348a057c1c8757ea3ed7
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-11-29 16:35:23 +0600

    Add files via upload

---
+++ Insertion in a Heap.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MinHeap {
+private:
+    vector<int> v;
+
+    void heapifyUp(int i) {
+        while (i > 0) {
+            int z = (i - 1) / 2;
+            if (v[i] < v[z]) {
+                swap(v[i], v[z]);
+                i = z;
+            } else {
+                break;
+            }
+        }
+    }
+
+public:
+    void insert(int a) {
+        v.push_back(a);
+        heapifyUp(v.size() - 1);  // Ensure this function is properly closed here
+    }
+
+    void printHeap() const {
+        for (int val : v) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    int N;
+    cin >> N;
+
+    MinHeap minHeap;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        minHeap.insert(a);
+        minHeap.printHeap();
+    }
+
+    return 0;
+}
