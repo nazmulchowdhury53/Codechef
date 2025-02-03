f601b172da436e962aa5b082e0559ab2c000483c
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2025-02-03 16:26:41 +0600

    Add files via upload

---
+++ Chef and Dolls.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+#include<vector>
+using namespace std;
+int main()
+{
+
+    long long int a,b,c,d,e,i,j;
+    cin>>a;
+    while(a--)
+    {
+
+        cin>>b;
+        for(i=0;i<b;i++)
+        {
+            cin>>d;
+
+
+
+           c=0;
+
+          c^=d;
+
+        }
+    }
+    cout<<c<<endl;
+}
